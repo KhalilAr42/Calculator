049c6bb8ff147d3c980a14770d948bb11e7cb8a2
khalilar42 <khalilarchkak@gmail.com>
2024-02-26 17:49:57 +0100

    Implement functions to evaluate a math expression
    Implement a function that will convert a mathematical expression into
    a postfix expression where operands come first then operations, And
    implement another function that take a postfix expression as argument
    and return the result. The use of postfix expression is to facilitate
    the parsing and evaluation of the user math input.

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ let firstNumber;
 let secondNumber;
 let operator;
 
+let operatorPriority = {
+    "+": "0",
+    "-": "0",
+    "*": "1",
+    "/": "1",
+};
+
+const lastResultDiv = document.querySelector("#lastResult");
+const buttons = document.querySelectorAll("button");
+
 function add(firstNumber, secondNumber) {
     return firstNumber + secondNumber;
 }
@@ -15,6 +25,9 @@ function multiply(firstNumber, secondNumber) {
 }
 
 function divide(firstNumber, secondNumber) {
+    if (secondNumber == 0) {
+        return "awili ?";
+    }
     return firstNumber / secondNumber;
 }
 
@@ -32,4 +45,114 @@ function operate(firstNumber, operator, secondNumber) {
             console.error(`This is not a valid operator : ${operator}`);
             break;
     }
+}
+
+function isValidMathOperation(expression) {
+    const pattern = /^\d+(\s*[\+\-\*\/]\s*\d+)*$/;
+    return pattern.test(expression);
+}
+
+function isOperator(char) {
+    return (
+        char == "+" ||
+        char == "-" ||
+        char == "*" ||
+        char == "x" ||
+        char == "/" ||
+        char == "%"
+    );
+}
+
+function isEmpty(array) {
+    return array.length === 0;
+}
+
+function isDigit(char) {
+    return /\d/.test(char);
+}
+
+function convertToPostfixExpression(expression) {
+    const formattedExpression = expression.replace(/\s/g, "");
+    let stack = [];
+    let postfixExpression = "";
+    let operand = "";
+
+    for (let i = 0; i < formattedExpression.length; i++) {
+        let char = formattedExpression[i];
+
+        if (isDigit(char)) {
+            operand += char;
+        } else if (isOperator(char)) {
+            if (operand !== "") {
+                postfixExpression += operand + " ";
+                operand = "";
+            }
+
+            while (!isEmpty(stack)) {
+                let lastElement = stack.length - 1;
+                let operator = stack[lastElement];
+                if (operatorPriority[operator] >= operatorPriority[char]) {
+                    postfixExpression += stack.pop() + " ";
+                } else {
+                    break;
+                }
+            }
+            stack.push(char);
+        }
+    }
+
+    if (operand !== "") {
+        postfixExpression += operand + " ";
+    }
+
+    while (!isEmpty(stack)) {
+        postfixExpression += stack.pop() + " ";
+    }
+
+    return postfixExpression.trim();
+}
+
+function calculatePostfixExpression(expression) {
+    let postfixExpression = convertToPostfixExpression(expression);
+    let stack = [];
+    let operand = "";
+    for (let i = 0; i < postfixExpression.length; i++) {
+        let char = postfixExpression[i];
+        if (isDigit(char)) {
+            operand += char;
+        } else if (char == " ") {
+            stack.push(parseInt(operand));
+            operand = "";
+        } else {
+            let number1 = stack.pop();
+            let number2 = stack.pop();
+            switch (char) {
+                case "+":
+                    stack.push(operate(number2, "+", number1));
+                    i++;
+                    break;
+
+                case "-":
+                    stack.push(operate(number2, "-", number1));
+                    i++;
+                    break;
+
+                case "/":
+                    stack.push(operate(number2, "/", number1));
+                    i++;
+                    break;
+
+                case "*":
+                    stack.push(operate(number2, "*", number1));
+                    i++;
+                    break;
+
+                default:
+                    console.log("WTF!");
+                    break;
+            }
+        }
+    }
+
+    return stack.pop();
 }
